d904c75033a83a514852f27838a5a879ef78cb89
Jodaniel Eusebio <133549697+Daniel0390@users.noreply.github.com>
2024-04-02 15:07:17 -0400

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -8,26 +8,26 @@ function calcular (){
    let resto  = parseFloat  (document.getElementById("elresto").value );
    let result = document.getElementById("alf_result").innerHTML= ancho - resto  ;
    let divisor= parseFloat (document.getElementById("eldivisor").value);    
-let ancho_total =document. (getElementById("alf_result2").innerHTML= result / divisor );
+let ancho_total =document.getElementById("alf_result2").innerHTML= result / divisor ;
 
     let alto = parseFloat(document.getElementById("valor2").value);
     let resto_de_alto = parseFloat(document.getElementById("elresto_alt").value);
-    let alto_total = (document.getElementById("jamb_result").innerHTML= alto - resto_de_alto);
+    let alto_total = document.getElementById("jamb_result").innerHTML= alto - resto_de_alto;
 
     let rieles = parseFloat(document.getElementById("valor1").value);
     let resto_rieles = parseFloat(document.getElementById("elrestorieles").value);
-    (document.getElementById("rieles_result").innerHTML= rieles - resto_rieles);
+    document.getElementById("rieles_result").innerHTML= rieles - resto_rieles;
     
      let latera = parseFloat(document.getElementById("valor2").value);
      let  resto_laerale=parseFloat(document.getElementById("elrestolate").value);
-     (document.getElementById("laterales_result").innerHTML= latera - resto_laerale);
+     document.getElementById("laterales_result").innerHTML= latera - resto_laerale;
 
 
      let achovid = parseFloat(document.getElementById("restodeanchodevidrio").value);
-     (document.getElementById("Vidr.Anch_result").innerHTML= ancho_total - achovid ); 
+     document.getElementById("Vidr.Anch_result").innerHTML= ancho_total - achovid ; 
 
      let altovid = parseFloat(document.getElementById("restodealtodevidrio").value);
-      (document.getElementById("Vidr.Alt_result").innerHTML= alto_total - altovid ); 
+      document.getElementById("Vidr.Alt_result").innerHTML= alto_total - altovid ; 
 
       
 }
