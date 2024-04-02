e39cd6771b0165237cfe5cd0f13b56659f000bc6
Jodaniel Eusebio <133549697+Daniel0390@users.noreply.github.com>
2024-04-02 14:59:35 -0400

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -7,27 +7,27 @@ function calcular (){
    let ancho  = parseFloat  (document.getElementById("valor1").value );
    let resto  = parseFloat  (document.getElementById("elresto").value );
    let result = document.getElementById("alf_result").innerHTML= ancho - resto  ;
-   let divisor= parseFloat(document.getElementById("eldivisor").value);    
-let ancho_total =document.getElementById("alf_result2").innerHTML= result / divisor ;
+   let divisor= parseFloat (document.getElementById("eldivisor").value);    
+let ancho_total =document. Number(getElementById("alf_result2").innerHTML= result / divisor ).tofixed(2);
 
     let alto = parseFloat(document.getElementById("valor2").value);
     let resto_de_alto = parseFloat(document.getElementById("elresto_alt").value);
-    let alto_total = (document.getElementById("jamb_result").innerHTML= alto - resto_de_alto)
+    let alto_total = Number(document.getElementById("jamb_result").innerHTML= alto - resto_de_alto).tofixed(2);
 
     let rieles = parseFloat(document.getElementById("valor1").value);
     let resto_rieles = parseFloat(document.getElementById("elrestorieles").value);
-    (document.getElementById("rieles_result").innerHTML= rieles - resto_rieles);
+    Number(document.getElementById("rieles_result").innerHTML= rieles - resto_rieles).tofixed(2);
     
      let latera = parseFloat(document.getElementById("valor2").value);
      let  resto_laerale=parseFloat(document.getElementById("elrestolate").value);
-     (document.getElementById("laterales_result").innerHTML= latera - resto_laerale);
+     Number(document.getElementById("laterales_result").innerHTML= latera - resto_laerale)tofixed(2);
 
 
      let achovid = parseFloat(document.getElementById("restodeanchodevidrio").value);
-     (document.getElementById("Vidr.Anch_result").innerHTML= ancho_total - achovid ); 
+     Number(document.getElementById("Vidr.Anch_result").innerHTML= ancho_total - achovid )tofixed(2); 
 
      let altovid = parseFloat(document.getElementById("restodealtodevidrio").value);
-     (document.getElementById("Vidr.Alt_result").innerHTML= alto_total - altovid ); 
+      Number(document.getElementById("Vidr.Alt_result").innerHTML= alto_total - altovid )tofixed(2); 
 
       
 }
